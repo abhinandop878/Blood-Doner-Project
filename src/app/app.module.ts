7f794df8f33e3adce6ce5c0176e0115f8836861b
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-09 14:06:49 +0530

    search doner program added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddDonerComponent } from './add-doner/add-doner.component';
+import { SearchDonerComponent } from './search-doner/search-doner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddDonerComponent
+    AddDonerComponent,
+    SearchDonerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/search-doner/search-doner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-doner',
+  templateUrl: './search-doner.component.html',
+  styleUrls: ['./search-doner.component.css']
+})
+export class SearchDonerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
