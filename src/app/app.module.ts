6366bbf51fe18b1cc0c66fc71be4e07ec07abb50
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-09 14:00:25 +0530

    add doner program added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddDonerComponent } from './add-doner/add-doner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDonerComponent
   ],
   imports: [
     BrowserModule,
