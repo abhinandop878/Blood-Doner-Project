86c9131fc91612a8b46cfb119540af392b8c3b2d
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-13 15:54:24 +0530

    form

--- src/app/add-doner/add-doner.component.ts
+++ src/app/add-doner/add-doner.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class AddDonerComponent implements OnInit {
 
   constructor() { }
+  donorName=""
+  bloodGroup=""
+  dob=""
+  lastDonorData=""
+  anyPreviousDiseases=""
+  address=""
+  pincode=""
+  mobileNo=""
+  emailID=""
 
+  readValue=()=>{
+    let data={
+      "donorName":this.donorName,
+      "bloodGroup":this.bloodGroup,
+      "dob":this.dob,
+      "lastDonorData":this.lastDonorData,
+      "anyPreviousDiseases":this.anyPreviousDiseases,
+      "address":this.address,
+      "pincode":this.pincode,
+      "mobileNo":this.mobileNo,
+      "emailID":this.emailID
+    }
+    console.log(data)
+  }
   ngOnInit(): void {
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewAllComponent } from './view-all/view-all.component';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 const appRoutes:Routes=[
 {
   path:"",component:AddDonerComponent
@@ -37,7 +38,8 @@ const appRoutes:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
